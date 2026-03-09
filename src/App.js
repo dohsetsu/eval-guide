d1417699df3f042821a20429afd743acf8f420d5
sbice-mines <jason_bice@intuit.com>
2026-03-09 15:44:22 -0700

    added analytics (i think:sweating:

--- src/App.js
+++ src/App.js
@@ -3,3 +3,22 @@ import EvalGuide from "./EvalGuide";
 export default function App() {
   return <EvalGuide />;
 }
+import { Analytics } from '@vercel/analytics/next';
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
